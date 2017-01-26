c0e49bcac5269a8998e7f85d149b659c7f09191e
Diego Gonzalez <gonzalezdiego93@outlook.com>
2017-01-25 20:52:31 -0500

    My first commit.

--- I Love Cheese/ViewController.swift
+++ I Love Cheese/ViewController.swift
@@ -10,9 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var TheLabel: UILabel!
+    
+    var TapCount = 0
+    
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+        
+        TapCount = TapCount + 1
+        print (TapCount)
+        if TapCount >= 5{
+            TheLabel.text = "Congrats fgt, kys"
+        }
+        
+    }
+    
+    
+    @IBAction func buttonTapped2(_ sender: Any) {
+        
+    TheLabel.text = "Buttons Are Cool!"
+        
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
